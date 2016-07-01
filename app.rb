de3efd3458b7e8ebe2db17c739396157d1266c8d
Stephanie Gurung <stephr3@gmail.com>
2016-07-01 09:57:01 -0700

    pass integration spec processes the user target word, counts how many times the target word appears in a given text, and returns this count

--- app.rb
+++ app.rb
@@ -1,8 +1,15 @@
 require('sinatra')
 require('sinatra/reloader')
-# require('count_words_in')
+require('count_words_in')
 also_reload('lib/**/*.rb')
 
 get('/') do
   erb(:index)
+end
+
+get('/count') do
+  @target_word = params.fetch('target_word')
+  @user_text = params.fetch('user_text')
+  @count = (@target_word).count_words_in(@user_text)
+  erb(:count)
 end
