83427f068315a1fd38302b660d39b01a9fb23d02
Stephanie Gurung <stephr3@gmail.com>
2016-07-01 11:58:06 -0700

    add javascript functionality to display an image corresponding to count number

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,5 +1,12 @@
-$(function(){
-  $('form#user_input').submit(function(){
-    alert('jquery is working')
-  })
-})
+// $(function(){
+//   $('form#user_input').submit(function(){
+//     alert('jquery is working')
+//     if (num === 0) {
+//       $('#add-img').append('<img src="img/count-0.jpg">')
+//     }else if (num === 1) {
+//       $('#add-img').append('<img src="img/count-1.jpg">')
+//     } else {
+//       $('#add-img').append('<img src="img/count-main.jpg">')
+//     }
+//   };
+// })
