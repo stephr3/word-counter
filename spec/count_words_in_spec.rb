a3fc4e35ebaa652d148bba65109c873716651276
Stephanie Gurung <stephr3@gmail.com>
2016-07-01 08:29:29 -0700

    add expected input and output for each spec in count_words_in_spec file

--- spec/count_words_in_spec.rb
+++ spec/count_words_in_spec.rb
@@ -3,30 +3,30 @@ require('count_words_in')
 
 describe('String#count_words_in') do
   it('returns 0 when two single words are compared and the words are not the same') do
-    expect().to()
+    expect('pet'.count_words_in('dog')).to(eq(0))
   end
   it('returns 1 when two single words are compared and the words are the same') do
-    expect().to()
+    expect('pet'.count_words_in('pet')).to(eq(1))
   end
   it('returns 0 when a word is compared with a string of words and the target word is not included in the string of words') do
-    expect().to()
+    expect('pet'.count_words_in('dog store')).to(eq(0))
   end
   it('returns 1 when a word is compared with a string of words and the target word is included in the string of words') do
-    expect().to()
+    expect('pet'.count_words_in('pet store')).to(eq(1))
   end
   it('can handle multiple occurrences of a word within a string of words') do
-    expect().to()
+    expect('pet'.count_words_in('pet a pet dog in the pet store')).to(eq(3))
   end
   it('can handle capitalized user entries') do
-    expect().to()
+    expect('pet'.count_words_in('Pet a pet dog in the Pet Store')).to(eq(3))
   end
   it('can handle mixed case user entries') do
-    expect().to()
+    expect('pet'.count_words_in('Pet a PET dOg in the pEt STOre')).to(eq(3))
   end
   it('can handle user entries with numbers') do
-    expect().to()
+    expect('pet'.count_words_in('Pet 2 pet dogs in the pet store')).to(eq(3))
   end
   it('can handle user entries with punctuation') do
-    expect().to()
+    expect('pet'.count_words_in('Pet 2 pet dogs in the pet store!!!')).to(eq(3))
   end
 end
