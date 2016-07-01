e544090b54356c53f2e56513c71c59b64dd46538
Stephanie Gurung <stephr3@gmail.com>
2016-07-01 08:23:02 -0700

    initialize English specifications in count_words_in_spec file

--- spec/count_words_in_spec.rb
+++ spec/count_words_in_spec.rb
@@ -1,2 +1,32 @@
 require('rspec')
 require('count_words_in')
+
+describe('String#count_words_in') do
+  it('returns 0 when two single words are compared and the words are not the same') do
+    expect().to()
+  end
+  it('returns 1 when two single words are compared and the words are the same') do
+    expect().to()
+  end
+  it('returns 0 when a word is compared with a string of words and the target word is not included in the string of words') do
+    expect().to()
+  end
+  it('returns 1 when a word is compared with a string of words and the target word is included in the string of words') do
+    expect().to()
+  end
+  it('can handle multiple occurrences of a word within a string of words') do
+    expect().to()
+  end
+  it('can handle capitalized user entries') do
+    expect().to()
+  end
+  it('can handle mixed case user entries') do
+    expect().to()
+  end
+  it('can handle user entries with numbers') do
+    expect().to()
+  end
+  it('can handle user entries with punctuation') do
+    expect().to()
+  end
+end
