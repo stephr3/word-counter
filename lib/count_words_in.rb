d9fad4e349380aae692608c58820cbde57298f5e
Stephanie Gurung <stephr3@gmail.com>
2016-07-01 09:00:02 -0700

    pass count_in_words spec returns 0 when a word is compared with a string of words and the target word is not included in the string of words

--- lib/count_words_in.rb
+++ lib/count_words_in.rb
@@ -3,8 +3,12 @@ require('pry')
 class String
   define_method(:count_words_in) do |string_of_words|
     count = 0
-    if self.==(string_of_words)
-      count = count.+(1)
+    word = self
+    string_of_words_array = string_of_words.split()
+    string_of_words_array.each() do |string_word|
+      if word.==(string_word)
+        count = count.+(1)
+      end
     end
   count
   end
